3b20788a3f1bd1333976243203d630a44add7007
muziling <muziling@users.noreply.github.com>
2024-07-25 22:10:43 +0800

    Update build-RPMs-OpenSSH-CentOS.sh

--- build-RPMs-OpenSSH-CentOS.sh
+++ build-RPMs-OpenSSH-CentOS.sh
@@ -90,6 +90,9 @@ build_RPMs() {
     sed -i -e "s/PreReq: initscripts >= 5.00/#PreReq: initscripts >= 5.00/g" openssh.spec
     sed -i -e "s/BuildRequires: openssl-devel < 1.1/#BuildRequires: openssl-devel < 1.1/g" openssh.spec
     sed -i -e "/check-files/ s/^#*/#/" /usr/lib/rpm/macros
+    rm -f openssh.spec
+    wget https://raw.githubusercontent.com/muziling/openssh-rpms/main/el7/SPECS/openssh.spec
+    chown root.root openssh.spec
     # https://github.com/muziling/openssh-rpms/blob/main/version.env
     rpmbuild -ba openssh.spec \
 		--define "opensslver 3.0.14" \
