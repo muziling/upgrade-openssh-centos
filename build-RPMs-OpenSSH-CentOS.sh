88531772246d0919a3cf7ae07f72311539dcde4c
muziling <muziling@users.noreply.github.com>
2024-10-28 15:25:34 +0800

    Update build-RPMs-OpenSSH-CentOS.sh

--- build-RPMs-OpenSSH-CentOS.sh
+++ build-RPMs-OpenSSH-CentOS.sh
@@ -73,7 +73,7 @@ build_RPMs() {
     wget --no-check-certificate -c https://cloudflare.cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-${version}.tar.gz
     wget --no-check-certificate -c https://cloudflare.cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-${version}.tar.gz.asc
     wget --no-check-certificate -c https://src.fedoraproject.org/repo/pkgs/openssh/x11-ssh-askpass-1.2.4.1.tar.gz/md5/8f2e41f3f7eaa8543a2440454637f3c3/x11-ssh-askpass-1.2.4.1.tar.gz
-    wget --no-check-certificate -c https://www.openssl.org/source/openssl-3.0.14.tar.gz
+    wget --no-check-certificate -c https://www.openssl.org/source/openssl-3.4.0.tar.gz
     wget --no-check-certificate -c https://www.cpan.org/src/5.0/perl-5.38.2.tar.gz
     
     tar zxvf openssh-${version}.tar.gz
